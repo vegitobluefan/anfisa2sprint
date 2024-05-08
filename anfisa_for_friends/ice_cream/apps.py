2dd138fd83d07ef5cf14a63e81e679f0b425b62b
vegitobluefan <daniararinov995@gmail.com>
2024-05-08 20:35:25 +0300

    commit lol

--- anfisa_for_friends/ice_cream/apps.py
+++ anfisa_for_friends/ice_cream/apps.py
@@ -1,6 +1,7 @@
-from django.apps import AppConfig
+from django.apps import AppConfig # type: ignore
 
 
 class IceCreamConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ice_cream'
+    verbose_name = 'Каталог мороженого'
